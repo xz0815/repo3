1f9d4e300dbcdafa55f4b2fc3e6a72ecc7297ad2
xuzhuo <974193084@qq.com>
2018-10-15 11:37:39 +0800

    新修改

--- git_test/src/com/itheima/git/FirstGit.java
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -3,11 +3,13 @@ package com.itheima.git;
 public class FirstGit {
 
     public static void main(String[] args) {
+
         System.out.println("hello git");
     }
 
     public void test(){
         int i = 10;
         i += 10;
+        System.out.println(i);
     }
 }
