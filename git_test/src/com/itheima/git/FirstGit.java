9ed20f7876afec2830635a8e9f664296e7592786
xuzhuo <974193084@qq.com>
2018-10-15 10:26:24 +0800

    把修改后工程添加到git

--- git_test/src/com/itheima/git/FirstGit.java
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -5,4 +5,9 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public void test(){
+        int i = 10;
+        i += 10;
+    }
 }
